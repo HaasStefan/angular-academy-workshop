083ed95acfbbc7f91bd0875c55a1760b601bf3c7
Stefan Haas <stefan.haas.privat@gmail.com>
2022-12-14 20:27:55 +0100

    .

--- apps/flight-app/src/app/flight-lookahead/flight-lookahead.component.ts
+++ apps/flight-app/src/app/flight-lookahead/flight-lookahead.component.ts
@@ -1,27 +1,91 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Flight } from '@flight-workspace/flight-lib';
-import { Observable } from 'rxjs';
+import {
+  BehaviorSubject,
+  catchError,
+  combineLatest,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  interval,
+  map,
+  Observable,
+  of,
+  ReplaySubject,
+  share,
+  startWith,
+  Subject,
+  Subscription,
+  switchMap,
+  takeUntil,
+  tap,
+} from 'rxjs';
 
 @Component({
   selector: 'flight-workspace-flight-lookahead',
   templateUrl: './flight-lookahead.component.html',
   styleUrls: ['./flight-lookahead.component.css'],
 })
-export class FlightLookaheadComponent implements OnInit {
+export class FlightLookaheadComponent implements OnInit, OnDestroy {
   control!: FormControl;
   flights$!: Observable<Flight[]>;
-  loading = false;
+
+  private loadingSubject = new BehaviorSubject(false);
+  readonly loading$ = this.loadingSubject.asObservable();
+
+  online$!: Observable<boolean>;
+
+  subscription = new Subscription();
+  destroy = new Subject<void>();
+
+  obs$ = interval(1000).pipe(tap(console.log));
 
   constructor(private http: HttpClient) {}
+
+  ngOnDestroy(): void {
+    //this.subscription.unsubscribe();
+    this.destroy.next();
+  }
 
   ngOnInit(): void {
     this.control = new FormControl();
 
+    this.online$ = interval(2000).pipe(
+      startWith(0),
+      map(() => Math.random() > 0.5), // f, f, f, t, t, f
+      distinctUntilChanged(),
+      //shareReplay({bufferSize: 1, refCount: true}),
+      share({
+        connector: () => new ReplaySubject(1),
+      })
+    );
+
+    const input$ = this.control.valueChanges.pipe(
+      debounceTime(900),
+      distinctUntilChanged()
+    );
+
+    const obs$ = interval(1000).pipe(tap(console.log));
+
+    ///obs$.pipe(takeUntil(this.destroy)).subscribe();
+
+    //this.subscription.add(
+    //  obs$.subscribe()
+    //);
+
+    this.flights$ = combineLatest({
+      online: this.online$,
+      input: input$,
+    }).pipe(
+      filter(({ online }) => online),
+      tap(() => this.loadingSubject.next(true)),
+      switchMap(({ input }) => this.load(input)),
+      tap(() => this.loadingSubject.next(false))
+    );
   }
 
-  
   load(from: string): Observable<Flight[]> {
     const url = 'http://www.angular.at/api/flight';
 
@@ -32,6 +96,11 @@ export class FlightLookaheadComponent implements OnInit {
 
     const headers = new HttpHeaders().set('Accept', 'application/json');
 
-    return this.http.get<Flight[]>(url, { params, headers });
+    return this.http.get<Flight[]>(url, { params, headers }).pipe(
+      catchError((error) => {
+        console.error('error', error);
+        return of([]);
+      })
+    );
   }
 }
